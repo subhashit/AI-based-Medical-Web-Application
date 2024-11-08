d77286cf584718fc6a51cef753d508e2a1e7d90d
Nikhil Kathait <nikhilkathait06@gmail.com>
2024-11-08 12:35:39 +0530

    Brain tumor model has been completed

--- medical_app/app.py
+++ medical_app/app.py
@@ -1,7 +1,52 @@
-from flask import Flask, render_template
+import os
+import cv2
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+from keras.models import load_model
+from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template
 
 # Initialize the Flask app
 app = Flask(__name__)
+
+model = load_model('BrainTumor.h5')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+def get_className(classNo):
+    if classNo == 0:
+        return "No Brain Tumor"
+    elif classNo == 1:
+        return "Yes Brain Tumor"
+
+def getResult(img):
+    image = cv2.imread(img)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((64,64))
+    image = np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result = model.predict(input_img)
+    predicted_class = np.argmax(result, axis = 1)
+    return predicted_class
+
+@app.route('/index', methods = ['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        value=getResult(file_path)
+        result=get_className(value) 
+        return result
+    return None
 
 # Home Route
 @app.route('/')
